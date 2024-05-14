e40fd980200043d8e2bc2b30ccd562e5b8547488
Nafis <nafisfragern@gmail.com>
2024-05-14 17:35:18 +0700

    Initial Commit

---
+++ nano-two Watch App/nano_twoApp.swift
@@ -0,0 +1,17 @@
+//
+//  nano_twoApp.swift
+//  nano-two Watch App
+//
+//  Created by Nafis-Macbook on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct nano_two_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
