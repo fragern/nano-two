c22915a10cbafee52c69770430efe960c6277bf0
Nafis <nafisfragern@gmail.com>
2024-05-18 15:25:21 +0700

    Update 18 May - [✅] detecting user location - [✅] use MVC structure - [❓] checking the GameView because it sometimes bugs - [❌] GameView.swift -> make it dynamic within the dedicated location and challenge - [❌] adding the progress of the user - [❌] HomeView.swift -> adding the progress bar - [❌] HomeView.swift -> convert the latitude and longitude into distance

---
+++ nano-two Watch App/Model/LocationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationViewModel.swift
+//  nano-two Watch App
+//
+//  Created by Nafis-Macbook on 18/05/24.
+//
+
+import Foundation
---
+++ nano-two Watch App/View/LocationTestTwoView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationTestTwoView.swift
+//  nano-two Watch App
+//
+//  Created by Nafis-Macbook on 18/05/24.
+//
+
+import SwiftUI
+
+struct LocationTestTwoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationTestTwoView()
+}
