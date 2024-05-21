23efd338c85fcf804ed976041545097c985f7fbc
Nafis <nafisfragern@gmail.com>
2024-05-21 13:46:11 +0700

    Update 21 May - [✅] The UI/UX already updated according to kak Ica's suggestion - [✅] Giving the sorting of the nearest location with the user - [✅] InstructionView.swift page done - [❌] Adding instruction button on the top of the page - [❌] Storing the user progress

---
+++ nano-two Watch App/View/InstructionView.swift
@@ -0,0 +1,18 @@
+//
+//  InstructionView.swift
+//  nano-two Watch App
+//
+//  Created by Nafis-Macbook on 21/05/24.
+//
+
+import SwiftUI
+
+struct InstructionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InstructionView()
+}
